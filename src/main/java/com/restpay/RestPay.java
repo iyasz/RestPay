ebbe792efef2e96190fe084a627834e67ac792e3
iyasz <zakamaragames@gmail.com>
2026-04-29 09:31:40 +0700

    initial commit

---
+++ src/main/java/com/restpay/RestPay.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.restpay;
+
+/**
+ *
+ * @author iyasz
+ */
+public class RestPay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
